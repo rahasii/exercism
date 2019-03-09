f142e553260c7e1df93055ada7cd5bf09d80af8d
rahasii <>
2019-03-09 12:22:08 +0900

    PHP Track - Hamming - improve code

--- php/hamming/hamming.php
+++ php/hamming/hamming.php
@@ -1,26 +1,15 @@
 <?php
 
-//
-// This is only a SKELETON file for the "Hamming" exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
-function distance($a, $b)
+function distance($a, $b):int
 {
     if(strlen($a) !== strlen($b)){
         throw new InvalidArgumentException('DNA strands must be of equal length.');
     }
 
-    $length = strlen($a);
     $arrayA = str_split($a);
     $arrayB = str_split($b);
 
-    $count = 0;
-    for($i = 0; $i < $length; $i++){
-        if($arrayA[$i] !== $arrayB[$i]){
-            $count++;
-        }
-    }
+    $diff = array_diff_assoc($arrayA,$arrayB);
 
-    return $count;
+    return sizeof($diff);
 }
