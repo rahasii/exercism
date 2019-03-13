f315011de2ae1160a925692557756efe255e5560
rahasii <rahasii.2010@gmail.com>
2019-03-13 23:23:21 +0900

    PHP Track - bob - passed PHPUnitTest ずっと正規表現はだいたいコピペで済ませてきたゆえ、かなり時間がかかった。 もっと良い書き方があることは感じている。しかし、一旦違う問題がやりたい。それほどに疲れた。 ただ、正規表現レベルが上がったのは確か。

--- php/bob/bob.php
+++ php/bob/bob.php
@@ -6,15 +6,23 @@ class Bob
     function respondTo($word): string
     {
 
-        if (preg_match('/\?/', $word)) {
+        if (preg_match('/^[\s]*$/', $word)) {
+            return 'Fine. Be that way!';
+        }
+
+        if (preg_match('/^[A-Z|\s]+\?$/', $word)) {
+            return "Calm down, I know what I'm doing!";
+        }
+
+        if (preg_match('/\?[\s]*$/', $word)) {
             return "Sure.";
         }
 
         if (
-            preg_match('/^(?!Let)/', $word)
-            &&
-            (preg_match('/\!$/', $word)||preg_match('/^[^a-z][A-Z][!|\s|,|\d]*/', $word))
-        )
+            (preg_match('/\!$/', $word)
+            || preg_match('/^[A-Z]+$/', $word)
+            || preg_match('/^[^a-z][A-Z|\s]+[!|,|\d]*$/', $word)
+            ) && preg_match('/^(?!Let)/', $word))
         {
             return "Whoa, chill out!";
         }
